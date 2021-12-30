e2aef913c6234a12de3670ee62300112954f0a91
OscarSah <oscar@cybertekschool.com>
2021-12-30 05:42:37 +0300

    Spartan Data Driven Testing with Long type phone number

--- src/test/java/step_definitions/SpartanStepDefs.java
+++ src/test/java/step_definitions/SpartanStepDefs.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public class SpartanStepDefs {
 
-    String expectedPhoneNumber;
+    Long expectedPhoneNumber;
     @When("the User retrives the phone number for the spartan {string}")
     public void the_User_retrives_the_phone_number_for_the_spartan(String name) {
 
@@ -18,12 +18,12 @@ public class SpartanStepDefs {
 
         Map<String, Object> rowMap = DBUtils.getRowMap(query);
         System.out.println("rowMap = " + rowMap);
-        expectedPhoneNumber = (String) rowMap.get("PHONE");
+        expectedPhoneNumber = Long.valueOf((String) rowMap.get("PHONE"));
 
     }
 
-    @Then("the phone number from DB should be {string}")
-    public void the_phone_number_from_DB_should_be(String actualPhone) {
+    @Then("the phone number from DB should be {}")
+    public void the_phone_number_from_DB_should_be(Long actualPhone) {
 
         Assert.assertEquals("Phone numbers do NOT match",expectedPhoneNumber,actualPhone);
     }
--- src/test/resources/features/spartanDB.feature
+++ src/test/resources/features/spartanDB.feature
@@ -2,12 +2,12 @@ Feature: Spartan Database Functionality
 
   Scenario: Verify each spartans phone number
     When the User retrives the phone number for the spartan "Nels"
-    Then the phone number from DB should be "4218971348"
+    Then the phone number from DB should be 4218971348
 
   @wip @db
   Scenario Outline: Verify all the spartans phone numbers
     When the User retrives the phone number for the spartan "<name>"
-    Then the phone number from DB should be "<phone>"
+    Then the phone number from DB should be <phone>
     Examples:
       | name   | phone      |
       | Nels   | 4218971348 |
